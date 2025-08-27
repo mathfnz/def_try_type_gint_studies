90178731d15f21af3ffd3af03f31e61a6de18bd4
mathfnz_macbook <matheus.fnds20@gmail.com>
2025-08-26 21:07:39 -0300

    using try/except, def, list comprehension

---
+++ exe01.py
@@ -0,0 +1,28 @@
+# %% 
+# Leitura Simples: Escreva um script que abra o ficheiro producao_sensores.csv, leia o seu conteúdo e imprima cada linha no terminal.
+import csv
+
+csv_file: str = "base.csv"
+# lendo csv
+def csv_reader(csv_file_path: str)-> list:
+    try:
+        with open(csv_file, "r", encoding="UTF-8", newline='') as file:
+            csv_reader = csv.reader(file)
+            data = [row for row in csv_reader]
+        print(data)
+    except FileNotFoundError:
+        print(f"Error: the file {csv_file} was not found!")
+    except Exception as e:
+        print(f"Error: {e}")
+    return data
+# %%
+dados_lidos = csv_reader(csv_file)
+
+# %% 
+print(f"Temos {len(dados_lidos)} linhas")
+
+# %%
+type(dados_lidos)
+find_fails = [row for row in dados_lidos if "Falha" in row]
+print(find_fails)
+
---
+++ rascunho.py
@@ -0,0 +1,10 @@
+# %%
+
+lista: list = ['matheus', 'douglas', 'fabio']
+find: str = 'matheus'
+for row in lista:
+    if 'Matheus' in row:
+        print("achei")
+    else:
+        print("merda")
+        
